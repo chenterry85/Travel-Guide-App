bc14d137d0839c2a8d56eaa882d4ad7349384658
Terry Chen <chenterry85@gmail.com>
2020-02-01 11:23:08 +0800

    Initial Commit

---
+++ simpleTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleTableView
+//
+//  Created by Terry Chen on 2020/2/1.
+//  Copyright © 2020 Terry Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
